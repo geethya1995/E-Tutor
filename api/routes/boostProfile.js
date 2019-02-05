e69dd75453726e03d3240e4d30ab8960b42b1298
Geethya Tennakoon <36676130+geethya1995@users.noreply.github.com>
2019-02-05 09:52:32 +0530

    boost profile update

--- api/routes/boostProfile.js
+++ api/routes/boostProfile.js
@@ -10,46 +10,82 @@ router.post('/boostprofile', (req, res) =>{
      var sql = "select * from Tutor where Tutor.email='"+tutor+"' and Tutor.rate>=3";
      var sql2 = "insert into ProfileBoost(email, package, startDate, expiryDate) values('"+tutor+"','"+package+"', CURRENT_TIMESTAMP(), TIMESTAMPADD(DAY,7,CURRENT_TIMESTAMP()))";
      var sql3 = "select * from ProfileBoost where email = '"+tutor+"'";
+     var sql7 = "update Tutor set Tutor.priority = 5000 where Tutor.email = '"+tutor+"'";
+    
 
-     con.query(sql, (err, result) => {
+     con.query(sql, (err, result1) => {
         if(err){
-            throw err;
             res.json({
                success: false,
-               allowed: false,
-               response: 'error!' 
+               allowed: null,
+            //    response: 'error!' 
             });
-        }else if(result.length==0){
+        }else if(result1.length==0){
             res.json({
                success: false,
                allowed: false,
-               response: 'Insufficient Rating!' 
+            //    response: 'Insufficient Rating!' 
             });
         }else{
-            con.query(sql3, (err,result) =>{
-                if(err) throw err;
-                else if(result.length == 0){
-                    con.query(sql2, (err, result) =>{
+            con.query(sql3, (err,result2) =>{
+                if(err){
+                    res.json({
+                        success: false,
+                        allowed: null,
+                     //    response: 'error!' 
+                     });
+                }
+                else if(result2.length == 0){
+                    con.query(sql2, (err, result3) =>{
                         if(err){
-                            throw err;
                             res.json({
                                 success: false,
-                                allowed: true,
-                                response: 'DB entry error!'
+                                allowed: null,
+                                // response: 'DB entry error!'
                             });
                         }else{ 
+                            var today = new Date();
+                            today.setHours(0, 0, 0, 0);
+                            var expiry = new Date(new Date().getTime()+(7*24*60*60*1000));
+                            var remaining = Math.floor((expiry.valueOf() - today.valueOf()) / (1000*60*60*24));
+                            var user = {
+                                tutor: tutor,
+                                package:  package,
+                                start: today,
+                                end: expiry,
+                                remaining : remaining
+
+                             }
                             res.json({
                                 success: true,
                                 allowed: true,
-                                response: 'boosted!'
-                            });    
+                                user: user
+                                // response: 'boosted!'
+                            });
+                            con.query(sql7, (err) => {
+                                if(err){
+                                    res.json({
+                                        success: false,
+                                        allowed: null
+                                    });
+                                }
+                            });  
                         }
                     });
                 }else{
+                    var user = {
+                        tutor: result2[0].email,
+                        package:  result2[0].package,
+                        start: result2[0].startDate,
+                        end: result2[0].expiryDate,
+                        remaining: (result2[0].expiryDate.valueOf()-result2[0].startDate.valueOf())/(1000*3600*24)
+                    }
+                    //  console.log(user);
                     res.json({
                         success: true,
                         allowed: false,
-                        response: 'Profile is already boosted!'
+                        user: user
+                        // response: 'Profile is already boosted!'
                     }); 
                 }
             });
@@ -57,53 +93,6 @@ router.post('/boostprofile', (req, res) =>{
      });
 });
 
-/*
-resetting boost status after a week
-var ExpiryCheckupUpdate = setInterval(dailyCheck, 1000*60*60*24); 
-*/
 
-function DailyCheckup(){
-    var sql4 = "select * from ProfileBoost where ProfileBoost.expiryDate < CURRENT_TIMESTAMP";
-
-    con.query(sql4, (err, result) =>{
-        if(err){
-            throw err;     
-        } 
-        else{
-            for(var i=0; i<result.length; i++){
-                var sql5 = "delete from ProfileBoost where ProfileBoost.email = '"+result[i].email+"'";
-                // var sql6 = "update Tutor set Tutor.boostStatus=0 where Tutor.email='"+result[i].email+"'";
-
-                con.query(sql5, (err, result) => {
-                    if(err) throw err;
-                    else{
-                      console.log('deleted!');
-                    }
-                });
-
-                // con.query(sql6, (err, result) => {
-                //     if(err) throw err;
-                //     else{
-                //         console.log('boost status changed');
-                //     }
-                // });
-            } 
-        }     
-    });
-}
-
-
-// schedule.scheduleJob('0 0 * * *', DailyCheckup);
-
-// //checking for delays if any
-// var Delay = schedule.scheduleJob('0 0 * * *', function(fireDate){
-//     console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
-// });
-// */
  
 module.exports = router;
-
-
-
-
-
